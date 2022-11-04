8ffb892abbba1f11ea2d5f49cfa90de45441aafa
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-11-04 17:02:01 -0600

    Updating Ch 9 coursework

--- chapter_exercises/Chapter9/Ch9_TIY.py
+++ chapter_exercises/Chapter9/Ch9_TIY.py
@@ -54,3 +54,30 @@ lex.describe_user()
 lex.greet_user()
 
 #9-4
+class Restaurant: 
+    """Create Class for a Mexican restaurant called Wapos."""
+    def __init__(self, restaurant_name, cuisine_type):
+        """initialize name and type attributes"""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    def describe_restaurant(self):
+        """Print the two attributes"""
+        print(f"{self.restaurant_name.title()} serves {self.cuisine_type.title()} food")
+    def open_restaurant(self):
+        """Declare that the restaurant is open"""
+        print(f"{self.restaurant_name.title()} is now open")
+    def set_number_served(self):
+        """"attribute counting the number of served patrons"""
+        print(f"There has been {self.number_served} customers served.")
+    def increment_number_served(self, customers):
+        """Add the given amount to the number of customers served"""
+        self.number_served  += customers
+
+restaurant = Restaurant('wapos', 'mexican')
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+restaurant.number_served = 16
+restaurant.set_number_served()
+restaurant.increment_number_served(62)
+restaurant.number_served 
