38a74df4b1f9097af499632ee1219010d14c42df
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-11-06 11:31:15 -0700

    Adding Chapter 9 coursework progress

--- chapter_exercises/Chapter9/Ch9_TIY.py
+++ chapter_exercises/Chapter9/Ch9_TIY.py
@@ -133,36 +133,6 @@ allen.login_attempts
 print(f"Allen has now attempted to login {allen.login_attempts} times.")
 
 #9-6 Inherit class from Restaurant above. 
-class Restaurant: 
-    """Create Class for a Mexican restaurant called Wapos."""
-    def __init__(self, restaurant_name, cuisine_type):
-        """initialize name and type attributes"""
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
-        self.number_served = 0
-    def describe_restaurant(self):
-        """Print the two attributes"""
-        print(f"{self.restaurant_name.title()} serves {self.cuisine_type.title()} food")
-    def open_restaurant(self):
-        """Declare that the restaurant is open"""
-        print(f"{self.restaurant_name.title()} is now open")
-    def set_number_served(self):
-        """"attribute counting the number of served patrons"""
-        print(f"There has been {self.number_served} customers served.")
-    def update_number_served(self, customers):
-        """set the number of customers served to a given value"""
-        self.number_served = customers
-    def increment_number_served(self, customer):
-        """Add the given amount to the number of customers served"""
-        self.number_served  += customer
-
-restaurant = Restaurant('wapos', 'mexican')
-restaurant.describe_restaurant()
-restaurant.open_restaurant()
-restaurant.update_number_served(16)
-restaurant.set_number_served()
-restaurant.increment_number_served(62)
-restaurant.set_number_served() 
 class IceCreamStand(Restaurant):
     """New class, adding flavors as an attribute"""
     def __init__(self, restaurant_name, cuisine_type):
@@ -175,3 +145,123 @@ class IceCreamStand(Restaurant):
 #create instance and call method to list ice cream flavors
 show_flavors = IceCreamStand('wapos', 'mexican')
 show_flavors.list_flavors()
+
+#9-7
+class Admin(User):
+    """Admin account intaking User class settings, with additional changes unique to Admin users"""
+    def __init__(self, first_name, last_name, user_name, user_type, login_attempts):
+        """Initialize User attributes and adding Admin attributes"""
+        super().__init__(first_name, last_name, user_name, user_type, login_attempts)
+#add below instance for 9-8
+        self.privileges = Privileges()
+#(originally in 9-7, changed for 9-8)        self.privileges = ['can add post', 'can delete post', 'can ban user']
+#(originally in 9-7, changed for 9-8)    def show_privileges(self):
+#(originally in 9-7, changed for 9-8)        """Print out list of privileges for Admin users"""
+#(originally in 9-7, changed for 9-8)        print(f"As an admin user, you are able to do the following actions: {self.privileges}.")
+#(originally in 9-7, changed for 9-8)admin_privileges = Admin('cody', 'baggins', 'cbaggins', 'admin', '1')
+#(originally in 9-7, changed for 9-8)admin_privileges.show_privileges()
+
+#9-8
+class Privileges:
+    """Privileges class with only one attribute"""
+    def __init__(self, privileges = ['can add post', 'can delete post', 'can ban user']):
+        """list privileges"""
+        self.privileges = privileges
+    def show_privileges(self):
+        """Print out list of privileges for Admin users"""
+        print(f"As an admin user, you are able to do the following actions: {self.privileges}.")
+admin_privileges = Admin('cody', 'baggins', 'cbaggins','admin', '1')
+admin_privileges.privileges.show_privileges()
+
+#9-9
+class Car():
+    """A simple attempt to represent a car."""
+
+    def __init__(self, manufacturer, model, year):
+        """Initialize attributes to describe a car."""
+        self.manufacturer = manufacturer
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.manufacturer + ' ' + self.model
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+        
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=60):
+        """Initialize the batteery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+        
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 60:
+            range = 140
+        elif self.battery_size == 85:
+            range = 185
+            
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+#9-10 Importing the Restaurant class into that program file. Saved as restaurant.py
+
+
+    def upgrade_battery(self):
+        """Upgrade the battery if possible."""
+        if self.battery_size == 60:
+            self.battery_size = 85
+            print("Upgraded the battery to 85 kWh.")
+        else:
+            print("The battery is already upgraded.")
+    
+        
+class ElectricCar(Car):
+    """Models aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, manufacturer, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(manufacturer, model, year)
+        self.battery = Battery()
+
+
+print("Make an electric car, and check the battery:")
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+my_tesla.battery.describe_battery()
+
+print("\nUpgrade the battery, and check it again:")
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
+
+print("\nTry upgrading the battery a second time.")
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
