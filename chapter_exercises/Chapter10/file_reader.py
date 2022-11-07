dbe6ad72ad61be2a62ebee9b6f0772991fbbc776
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-11-07 13:12:32 -0700

    Chapter 10 coursework upload

---
+++ chapter_exercises/Chapter10/file_reader.py
@@ -0,0 +1,5 @@
+file_path = '/Users/bryanwenger/Desktop/python_work/python_crash_course/chapter_exercises/Chapter10/pi_digits.txt'
+with open(file_path) as file_object:
+    contents = file_object.read()
+print(contents)
+
