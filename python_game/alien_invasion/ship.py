1ab35b1a2f80b546363d261944c95ba9855af707
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-11-12 13:21:39 -0700

    Adjusting ship movement speed

--- python_game/alien_invasion/ship.py
+++ python_game/alien_invasion/ship.py
@@ -1,16 +1,19 @@
 import pygame
-
 class Ship(): 
     """A class to manage the ship"""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
+        # store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
 
         #Movement flags
         self.moving_right = False
@@ -21,10 +24,15 @@ class Ship():
     
     def update(self):
         """Update the ship's position based on the movement flags."""
+        #update the ship's x value, not the rect
         if self.moving_right:
-            self.rect.x += 1
+            self.rect.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.rect.x -= self.settings.ship_speed
+
+        #update rect object from self.x.
+        self.rect.x - self.x
+
     def blitme(self):
         """Draw the shp at its current location"""
         self.screen.blit(self.image, self.rect)
